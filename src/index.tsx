753b2b2287501cb4576df142f6818d12e7a6fafc
changlin-cn <changlin-cn@users.noreply.github.com>
2025-09-17 16:07:46 +0800

    chore(*): removed some pages

--- src/index.tsx
+++ src/index.tsx
@@ -6,23 +6,16 @@ import 'virtual:windi-devtools';
 import { Navigator } from './components/Navigator/Navigator';
 import { NotFound } from './pages/NotFound';
 import { RemoteCursorsOverlayPage } from './pages/RemoteCursorOverlay';
-import { SimplePage } from './pages/Simple';
-import { RemoteCursorDecorations } from './pages/RemoteCursorDecorations';
 
 ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route
-          path="/remote-cursors-overlay"
+          path="/"
           element={<RemoteCursorsOverlayPage />}
         />
-        <Route
-          path="/remote-cursors-decoration"
-          element={<RemoteCursorDecorations />}
-        />
-        <Route path="/simple" element={<SimplePage />} />
-        <Route path="/" element={<Navigate to="/remote-cursors-overlay" />} />
+       
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Navigator />
